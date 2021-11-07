189b3969af823834f1913751b9046c3591c121a6
Ferdinand Lischka <ferdinand.lischka@ngojobs.at>
2021-11-07 10:55:33 +0100

    added esc_attr() to escape $-variables

--- shortcode-job-package-button.php
+++ shortcode-job-package-button.php
@@ -10,14 +10,13 @@ function wpfr_add_job_package_button ( $atts = array() ) {
         'button_class' => ''
        ), $atts));
 
-
-    return "<div class=\"$div_class\"><form method=\"post\" id=\"job_package_selection\" action=\"$url\">
-    <input type=\"hidden\" name=\"job_id\" value=\"0\" />
-    <input type=\"hidden\" name=\"step\" value=\"0\" />
-    <input type=\"hidden\" name=\"job_manager_form\" value=\"submit-job\" />
+    return '<div class="' . esc_attr($div_class) . '"><form method="post" id="job_package_selection" action="' . esc_attr($url) . '">
+    <input type="hidden" name="job_id" value="0" />
+    <input type="hidden" name="step" value="0" />
+    <input type="hidden" name="job_manager_form" value="submit-job" />
     
-    <button class=\"$button_class\" type=\"submit\" name=\"job_package\" value=\"$id\" id=\"package-$id\">$text</button>
-    </form></div>";
+    <button class="' . esc_attr($button_class) . '" type="submit" name="job_package" value="' . esc_attr($id) . '" id="package-' . esc_attr($id) . '">' . esc_attr($text) . '</button>
+    </form></div>';
 } 
 
 add_shortcode('job_package_button', 'wpfr_add_job_package_button'); 
